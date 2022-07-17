b038fc026fa906963aa8c0c9e6f656c15242d229
Galatic308 <74140450+Galatic308@users.noreply.github.com>
2022-07-17 10:35:56 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,9 @@ class Test(MycroftSkill):
     @intent_file_handler('test.intent')
     def handle_test(self, message):
         self.speak_dialog('test')
-
+    def test_response(self, message):
+        self.message_intent('whats your favroite movie')
+        self.speak_dialog('curently the terminato r')
 
 def create_skill():
     return Test()
